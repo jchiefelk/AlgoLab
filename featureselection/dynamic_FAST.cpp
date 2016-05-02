f8905de701b13d4d7eae72490ce26f9d76ce9f0b
3LP <3levelproductions@gmail.com>
2016-05-01 18:16:45 -0700

    C++ - Dynamic FAST

--- featureselection/dynamic_FAST.cpp
+++ featureselection/dynamic_FAST.cpp
@@ -15,30 +15,38 @@ int main(int, char**)
     if(!cap.isOpened()){  
         return -1; // check if we succeeded
     }
-    Mat edges;
-    // namedWindow("edges",1);
-    // Sift Code
-    Mat src, descriptors,dest;
+    // cout << "rows :" << frame.rows << "\n";
+    Mat src, dest;
+    cout << src.rows << "\n";
     vector<KeyPoint> keypoints;
     vector<cv::KeyPoint>::iterator it;
     vector<cv::Point2f> points;
     ofstream outfile1,outfile2,outfile3;
-    int threshold=15;
+    int threshold=5;
     for(;;)
     {
         Mat frame;
         cap >> frame; // get a new frame from camera
-
+        // cout << "rows :" << frame.rows << "\n";
         // SIFT
         //
         src = frame;
         cvtColor(src, src, CV_BGR2GRAY);
-        // SIFT sift(500,1,0.1);
-        // sift(src, src, keypoints, descriptors, false);
+        GaussianBlur(src, src, Size(7,7), 1.5, 1.5);
+        // Canny(src, src, 0, 30, 3);
         FAST(src,keypoints,threshold,true);
         drawKeypoints(src, keypoints, dest);
-        imshow("Sift", dest);
- 
+        for(it=keypoints.begin(); it!=keypoints.end();it++){
+                    points.push_back(it->pt);
+        }
+        Mat pointmatrix(points);
+        outfile1.open("points.txt");
+        outfile1 << pointmatrix;
+        outfile1.close();
+        outfile3.open("src.txt");
+        outfile3 << src;
+        outfile3.close();
+        imshow("FAST", dest);
         if(waitKey(30) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
