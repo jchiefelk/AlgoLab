93ba078ef38845081162b70a2e99dd834b0c5005
3LP <3levelproductions@gmail.com>
2017-01-19 23:41:25 -0800

    Keypoints now stored as cv::Mat

--- VisionCode/VisionCode/main.cpp
+++ VisionCode/VisionCode/main.cpp
@@ -5,6 +5,7 @@
 //  Created by Jackson on 5/4/16.
 //  Copyright © 2016 Jackson. All rights reserved.
 //
+#include <fstream>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include "opencv2/highgui/highgui.hpp"
@@ -28,9 +29,9 @@ int main() {
         vector<KeyPoint> keypoints;
         vector<cv::KeyPoint>::iterator it;
         vector<cv::Point2f> points;
-        ofstream outfile1,outfile2,outfile3;
+        ofstream outfile1;
         int threshold=3;
-         // Read camera frames (at approx 24 FPS) and show the result
+        // Read camera frames (at approx 24 FPS) and show the result
          while(true) {
                 cap >> image;
                 if(image.empty()) {
@@ -39,10 +40,19 @@ int main() {
                 }
                 src = image;
                 cvtColor(src, src, CV_BGR2GRAY);
-             GaussianBlur(src, src, cv::Size(7,7), 1.5, 1.5);
+                GaussianBlur(src, src, cv::Size(7,7), 1.5, 1.5);
                 FAST(src,keypoints,threshold,true);
                 drawKeypoints(src, keypoints, dest);
                 imshow("Camera feed", dest);
+                // Print Keypoints to File
+             
+                for(it=keypoints.begin(); it!=keypoints.end();it++){
+                 points.push_back(it->pt);
+                }
+                Mat pointmatrix(points);
+                outfile1.open("points.txt");
+                outfile1 << pointmatrix;
+                outfile1.close();
                  // Stop the camera if users presses the "ESC" key
                  if(waitKey(1000.0/FPS) == 27) break;
              }
